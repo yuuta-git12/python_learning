231d495a55a9da2e7b648f5fcca5f6d14957b66d
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2024-02-04 14:11:03 +0900

    [update]Flake8構文修正

--- app/sample.py
+++ app/sample.py
@@ -1,10 +1,12 @@
 import math
 import sys
 
+
 def main():
-  val = float(sys.argv[1])
-  print(math.radians(val))
-  print('hello world')
+    val = float(sys.argv[1])
+    print(math.radians(val))
+    print('hello world')
+
 
 if __name__ == "__main__":
-  main()
+    main()
